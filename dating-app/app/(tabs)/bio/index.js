4efa85ad9d97717a0c735b72ac2526cd8eb11433
HiYi <hieu243268@gmail.com>
2024-02-01 18:24:48 -0800

    Display image by carsoul

--- dating-app/app/(tabs)/bio/index.js
+++ dating-app/app/(tabs)/bio/index.js
@@ -1,14 +1,278 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Pressable,
+  TextInput,
+  Button,
+  FlatList,
+  Alert,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Entypo, AntDesign } from "@expo/vector-icons";
+import Carousel from "react-native-snap-carousel";
 
 const index = () => {
+  const [option, setOption] = useState("AD");
+  const [description, setDescription] = useState("");
+  const [activeSlide, setActiveSlide] = React.useState(0);
+  const profileImages = [
+    {
+      image:
+        "https://images.pexels.com/photos/1042140/pexels-photo-1042140.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1215695/pexels-photo-1215695.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/7580971/pexels-photo-7580971.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+  ];
+  const turnons = [
+    {
+      id: "0",
+      name: "Music",
+      description: "Pop Rock-Indie pick our sound track",
+    },
+    {
+      id: "10",
+      name: "Kissing",
+      description:
+        " It's a feeling of closeness, where every touch of lips creates a symphony of emotions.",
+    },
+    {
+      id: "1",
+      name: "Fantasies",
+      description:
+        "Fantasies can be deeply personal, encompassing diverse elements such as romance",
+    },
+    {
+      id: "2",
+      name: "Nibbling",
+      description:
+        "playful form of biting or taking small, gentle bites, typically done with the teeth",
+    },
+    {
+      id: "3",
+      name: "Desire",
+      description: "powerful emotion or attainment of a particular person.",
+    },
+  ];
+  const data = [
+    {
+      id: "0",
+      name: "Casual",
+      description: "Let's keep it easy and see where it goes",
+    },
+    {
+      id: "1",
+      name: "Long Term",
+      description: "How about a one life stand",
+    },
+    {
+      id: "2",
+      name: "Virtual",
+      description: "Let's have some virtual fun",
+    },
+    {
+      id: "3",
+      name: "Open for Anything",
+      description: "Let's Vibe and see where it goes",
+    },
+  ];
+
+  const renderImage = ({ item }) => {
+    return (
+      <View
+      style={{ width: "100%", justifyContent: "center", alignItems: "center" }}
+    >
+        <Image
+          style={{
+            width: "85%",
+            resizeMode: "cover",
+            height: 290,
+            borderRadius: 10,
+            transform: [{ rotate: "-5deg" }],
+          }}
+          source={{ uri: item?.image }}
+        />
+        <Text
+          style={{ position: "absolute", top: 10, right: 10, color: "black" }}
+        >
+          {activeSlide + 1}/{profileImages.length}
+        </Text>
+      </View>
+    );
+  };
+  
+  
+
   return (
-    <View>
-      <Text>hello bio sssss</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <View>
+        <Image
+          style={{ width: "100%", height: 200, resizeMode: "cover" }}
+          source={{
+            uri: "https://static.vecteezy.com/system/resources/thumbnails/018/977/074/original/animated-backgrounds-with-liquid-motion-graphic-background-cool-moving-animation-for-your-background-free-video.jpg",
+          }}
+        />
+        <View>
+          <View>
+            <Pressable
+              style={{
+                padding: 10,
+                backgroundColor: "#DDA0DD",
+                width: 300,
+                marginLeft: "auto",
+                marginRight: "auto",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: 10,
+                position: "absolute",
+                top: -60,
+                left: "50%",
+                transform: [{ translateX: -150 }],
+              }}
+            >
+              <Image
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 30,
+                  resizeMode: "cover",
+                }}
+              />
+              <Text style={{ fontSize: 16, fontWeight: "600", marginTop: 6 }}>
+                Bangalore
+              </Text>
+              <Text style={{ marginTop: 4, fontSize: 15 }}>
+                22 years 110 days
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
 
-export default index
+      <View
+        style={{
+          marginTop: 80,
+          marginHorizontal: 20,
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 25,
+          justifyContent: "center",
+        }}
+      >
+        <Pressable onPress={() => setOption("AD")}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "500",
+              color: option == "AD" ? "black" : "gray",
+            }}
+          >
+            AD
+          </Text>
+        </Pressable>
+        <Pressable onPress={() => setOption("Photos")}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "500",
+              color: option == "Photos" ? "black" : "gray",
+            }}
+          >
+            Photos
+          </Text>
+        </Pressable>
+        <Pressable onPress={() => setOption("Turn-ons")}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "500",
+              color: option == "Turn-ons" ? "black" : "gray",
+            }}
+          >
+            Turn-ons
+          </Text>
+        </Pressable>
+        <Pressable onPress={() => setOption("Looking For")}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "500",
+              color: option == "Looking For" ? "black" : "gray",
+            }}
+          >
+            Looking For
+          </Text>
+        </Pressable>
+      </View>
+      <View style={{ marginHorizontal: 14, marginVertical: 15 }}>
+        {option == "AD" && (
+          <View
+            style={{
+              borderColor: "#202020",
+              borderWidth: 1,
+              padding: 10,
+              borderRadius: 10,
+              height: 300,
+            }}
+          >
+            <TextInput
+              value={description}
+              onChangeText={(text) => setDescription(text)}
+              style={{
+                fontFamily: "Helvetica",
+              }}
+              placeholder="Write your AD for people to like you"
+            />
+            <Pressable
+              style={{
+                marginTop: "auto",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 15,
+                backgroundColor: "black",
+                borderRadius: 5,
+                justifyContent: "center",
+                padding: 10,
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  fontSize: 15,
+                  fontWeight: "500",
+                }}
+              >
+                Publish in feed
+              </Text>
+              <Entypo name="mask" size={24} color="white" />
+            </Pressable>
+          </View>
+        )}
+      </View>
+      <View style={{ marginHorizontal: 14 }}>
+        {option == "Photos" && (
+          <Carousel
+            data={profileImages}
+            renderItem={renderImage}
+            sliderWidth={350}
+            itemWidth={300}
+            onSnapToItem={(index) => setActiveSlide(index)}
+          />
+        )}
+      </View>
+    </ScrollView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default index;
+
+const styles = StyleSheet.create({});
